f50d8939a8cc8d0f8ef11a5b409f1608cbbf22ad
dibakar102 <dibakar102@yahoo.com>
2017-08-04 14:10:54 -0700

    implementing border Style

--- app/src/main/java/com/example/androidu/musicmaker/ui/LoopEditorActivity.java
+++ app/src/main/java/com/example/androidu/musicmaker/ui/LoopEditorActivity.java
@@ -1,26 +1,8 @@
 package com.example.androidu.musicmaker.ui;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.GridLayout;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.androidu.musicmaker.R;
 import com.example.androidu.musicmaker.model.Instrument;
@@ -28,11 +10,9 @@ import com.example.androidu.musicmaker.model.Loop;
 import com.example.androidu.musicmaker.model.Note;
 import com.example.androidu.musicmaker.model.Tone;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LoopEditorActivity extends Activity {
 
+    LinearLayout parent;
     // These methods are called in response to a user action
     void onTonePlacement(Tone newTone){}
     void onPlayRequest(){}
@@ -45,242 +25,12 @@ public class LoopEditorActivity extends Activity {
 
     // These methods are used to control the activity
     void setLoop(Loop loop){}
-
-    private TextView mTvLoopName, mTvNoOfLoopsMeasure, mTvBeatsPerMeasure, mTvRytherZoomLevel;
-    private Spinner mSpInstruments;
-    private ImageView mImPlayPause;
-    private TextView mTvNotes, mTvInstrumentLegend;
-    private boolean play = true;
-    LinearLayout mLnNotes;
-    GridLayout gd;
-    private static int mNumberOfMeasures = 10, mNumberOfBeats = 4, mZoomLevel = 1;
-    int mMeasureCount = 1;
-    int mBeatCount = 2;
-
-    List<Integer> numberOfMeasuresList = new ArrayList<>();
-
-    TextView tv;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loop_editor);
 
-        mTvLoopName = (TextView) findViewById(R.id.tv_loopName);
-        mTvNoOfLoopsMeasure = (TextView) findViewById(R.id.tv_noLoopMeasure);
-        mTvBeatsPerMeasure = (TextView) findViewById(R.id.tv_beatsPerMeausre);
-        mTvRytherZoomLevel = (TextView) findViewById(R.id.tv_rytherZoomLevel);
-        mSpInstruments = (Spinner) findViewById(R.id.sp_instruments);
-        mTvInstrumentLegend = (TextView) findViewById(R.id.tv_spinner_legend);
-        mLnNotes = (LinearLayout) findViewById(R.id.ln_lyt);
 
-        onUserInput();
-
-        Context context = getApplicationContext();
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context, R.array.instruments, android.R.layout.simple_spinner_dropdown_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpInstruments.setAdapter(adapter);
-
-        mTvLoopName.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LayoutInflater layoutInflater = LayoutInflater.from(LoopEditorActivity.this);
-                final View newFileView = layoutInflater.inflate(R.layout.activity_new_filename, null);
-                AlertDialog.Builder newFileDialog = new AlertDialog.Builder(LoopEditorActivity.this);
-
-                newFileDialog.setTitle("Name of the file: ");
-                newFileDialog.setView(newFileView);
-
-                final EditText edFileName = (EditText) newFileView.findViewById(R.id.ed_filename);
-
-                newFileDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton)
-                    {
-                        String fileName = edFileName.getText().toString();
-                        mTvLoopName.setText("Loop Name: \n" + fileName);
-                    }
-                });
-
-                newFileDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        dialog.cancel();
-                    }
-                });
-                newFileDialog.show();
-            }
-        });
-
-        mTvNoOfLoopsMeasure.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LayoutInflater layoutInflater = LayoutInflater.from(LoopEditorActivity.this);
-                final View numberOfLoopView = layoutInflater.inflate(R.layout.activity_loop_numbers, null);
-                AlertDialog.Builder numLoopDialog = new AlertDialog.Builder(LoopEditorActivity.this);
-
-                numLoopDialog.setTitle("Number of loop measures: ");
-                numLoopDialog.setView(numberOfLoopView);
-
-                final EditText edNumLoop = (EditText) numberOfLoopView.findViewById(R.id.ed_loopNumbers);
-
-                numLoopDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton)
-                    {
-                        String loopNumbers = edNumLoop.getText().toString();
-                        mNumberOfMeasures = Integer.parseInt(loopNumbers);
-                        mTvNoOfLoopsMeasure.setText("Number of loop \n Measures: \n" + loopNumbers);
-                        numberOfMeasuresList.clear();
-                        onUserInput();
-                    }
-                });
-
-                numLoopDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        dialog.cancel();
-                    }
-                });
-                numLoopDialog.show();
-            }
-        });
-
-        mTvBeatsPerMeasure.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LayoutInflater layoutInflater = LayoutInflater.from(LoopEditorActivity.this);
-                final View numberOfLoopView = layoutInflater.inflate(R.layout.activity_loop_numbers, null);
-                AlertDialog.Builder numLoopDialog = new AlertDialog.Builder(LoopEditorActivity.this);
-
-                numLoopDialog.setTitle("Beats per measure: ");
-                numLoopDialog.setView(numberOfLoopView);
-
-                final EditText edNumLoop = (EditText) numberOfLoopView.findViewById(R.id.ed_loopNumbers);
-
-                numLoopDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton)
-                    {
-                        String beatPerMeasure = edNumLoop.getText().toString();
-                        mNumberOfBeats = Integer.parseInt(beatPerMeasure);
-                        mTvBeatsPerMeasure.setText("Number of loop \n Measures: \n" + beatPerMeasure);
-                    }
-                });
-
-                numLoopDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        dialog.cancel();
-                    }
-                });
-                numLoopDialog.show();
-            }
-        });
-
-        mSpInstruments.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if(mSpInstruments.getSelectedItem().toString().equals("Guitar")){
-                    mTvInstrumentLegend.setBackgroundColor(Color.RED);
-                } else if(mSpInstruments.getSelectedItem().toString().equals("Piano")){
-                    mTvInstrumentLegend.setBackgroundColor(Color.BLUE);
-                } else if(mSpInstruments.getSelectedItem().toString().equals("Recorder")){
-                    mTvInstrumentLegend.setBackgroundColor(Color.BLACK);
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-    }
-
-
-
-
-//    public void togglePlayPause(View view){
-//        final ImageView playPause = (ImageView) findViewById(R.id.im_playPause);
-//        playPause.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                if (play) {
-//                    playPause.setImageResource(R.drawable.ic_pause_circle_outline_black_24px);
-//                    play = false;
-//                } else {
-//                    playPause.setImageResource(R.drawable.ic_play_arrow_black_24px);
-//                    play = true;
-//                }
-//            }
-//        });
-//    }
-
-    public void onUserInput() {
-
-        String[] chord = {"C", "B", "Bb", "A", "Ab", "G", "F#", "F", "E", "Eb", "D", "C#", "C"};
-
-        for(int i = 1; i <= (mNumberOfMeasures * mNumberOfBeats); i++){
-            if(i % mNumberOfBeats == 1){
-                numberOfMeasuresList.add(mMeasureCount);
-                mMeasureCount++;
-                mBeatCount = 2;
-            } else{
-                numberOfMeasuresList.add(mBeatCount);
-                mBeatCount++;
-            }
-
-        }
-
-        for(int g = 0; g < 1; g++) {
-            gd = new GridLayout(this);
-            gd.setColumnCount((mNumberOfMeasures * mNumberOfBeats));
-            gd.setRowCount(chord.length);
-            mLnNotes.addView(gd);
-            int rowHeadCount = 0;
-            for (int i = 0; i < (mNumberOfMeasures * mNumberOfBeats * chord.length); i++) {
-                int check = mNumberOfMeasures * mNumberOfBeats;
-                tv = new TextView(this);
-                tv.setContentDescription("id: " + i);
-                tv.setHeight(100);
-                tv.setWidth(100);
-                tv.setId(i);
-                tv.setBackgroundColor(getResources().getColor(R.color.buttonColor));
-                if (tv.getId() < (mNumberOfMeasures * mNumberOfBeats) && tv.getId() != 0) {
-                    tv.setText(String.valueOf(numberOfMeasuresList.get(i-1)));
-                } else if (tv.getId() >= (mNumberOfMeasures * mNumberOfBeats) && tv.getId() % check == 0) {
-                    check = check + mNumberOfMeasures * mNumberOfBeats;
-                    tv.setText(chord[rowHeadCount]);
-                    rowHeadCount++;
-                    Log.d("TAG", "count: " + rowHeadCount);
-                } else if(tv.getId() != 0){
-                    tv.setOnTouchListener(new View.OnTouchListener() {
-                        @Override
-                        public boolean onTouch(View v, MotionEvent event) {
-                            if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                                tvClick(v);
-                                return true;
-                            }
-                            return false;
-                        }
-                    });
-                }
-                gd.addView(tv);
-            }
-        }
-        onStart();
-    }
-
-
-
-    public void tvClick(View view){
-        ColorDrawable col = (ColorDrawable) view.getBackground();
-        int colorCode = col.getColor();
-        if(colorCode == getColor(R.color.buttonColor)){
-            if(mSpInstruments.getSelectedItem().toString().equals("Guitar")){
-                view.setBackgroundColor(Color.RED);
-            } else if(mSpInstruments.getSelectedItem().toString().equals("Piano")){
-                view.setBackgroundColor(Color.BLUE);
-            } else if (mSpInstruments.getSelectedItem().toString().equals("Recorder")){
-                view.setBackgroundColor(Color.BLACK);
-            }
-        } else {
-            view.setBackgroundColor(getColor(R.color.buttonColor));
-        }
-        Toast.makeText(getApplicationContext(), view.getContentDescription(), Toast.LENGTH_LONG).show();
     }
 }
 
