ab271af8ca132cd1c7b0a4b89ebf7faa66dae88a
yikesitschris <yikesitschris@gmail.com>
2017-08-05 23:59:17 -0700

    saverloader update

--- app/src/main/java/com/example/androidu/musicmaker/filesystem/SaverLoader.java
+++ app/src/main/java/com/example/androidu/musicmaker/filesystem/SaverLoader.java
@@ -4,14 +4,23 @@ import com.example.androidu.musicmaker.audio.test.TestSongs;
 import com.example.androidu.musicmaker.model.Song;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
+
+import android.content.Context;
 import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 public class SaverLoader {
 
     public static final String tag = "jsonTEST";
 
-    public static boolean save(Song song, String filename) {
+    public static boolean save(Song song, String filename, Context context) {
 
         ObjectMapper mapper = new ObjectMapper();
         String json = "";
@@ -19,15 +28,50 @@ public class SaverLoader {
             json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(song);
 //            json = mapper.writeValueAsString(song);
 //            System.out.println(json);
-            Log.d(tag, json);
+//            Log.d(tag, json);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
+        }
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(filename, Context.MODE_PRIVATE));
+            outputStreamWriter.write(json);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
         }
 
         return true;
     }
 
-    public static boolean load(Song song, String filename) {
+    public static boolean load(Song song, String filename, Context context) {
+        String json = "";
+
+        try {
+            InputStream inputStream = context.openFileInput(filename);
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                json = stringBuilder.toString();
+
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        Log.d(tag, json);
         return true;
     }
 }
